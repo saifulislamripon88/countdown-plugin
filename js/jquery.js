963762eb238278549987597636e5d0af15691395
saifulislamripon88 <si3491061@gmail.com>
2022-06-26 02:44:47 +0600

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  window.onload = ()=>{
+    // $(selector).countMe(delay,speed)
+    $("#num1").countMe(40,65);
+    $("#num2").countMe(30, 30);
+    $("#num3").countMe(40, 50);
+    $("#num4").countMe(100,200);
+  }
+  
+
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+
+});
+
